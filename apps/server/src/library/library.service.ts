120b97355db6a18db4d82102937e2ca01748d3c5
ash <ash>
2024-09-21 21:14:13 +0530

    [feat-library]: adds return types

--- apps/server/src/library/library.service.ts
+++ apps/server/src/library/library.service.ts
@@ -2,11 +2,12 @@ import { Injectable } from '@nestjs/common';
 import { CreateLibraryDto } from './dto/create-library.dto';
 import { UpdateLibraryDto } from './dto/update-library.dto';
 import LibraryRepository from './repository/library.repository';
+import Library from './schema/library.schema';
 
 @Injectable()
 export class LibraryService {
-  constructor(private readonly repository: LibraryRepository){}
-  create(createLibraryDto: CreateLibraryDto) {
+  constructor(private readonly repository: LibraryRepository) {}
+  create(createLibraryDto: CreateLibraryDto): Promise<Library> {
     return this.repository.create(createLibraryDto);
   }
 
@@ -14,15 +15,15 @@ export class LibraryService {
     return `This action returns all library`;
   }
 
-  findOne(id: string) {
+  findOne(id: string): Promise<Library> {
     return this.repository.findById(id);
   }
 
-  update(id: string, updateLibraryDto: UpdateLibraryDto) {
+  update(id: string, updateLibraryDto: UpdateLibraryDto): Promise<Library> {
     return this.repository.findByIdAndUpdate(id, updateLibraryDto);
   }
 
-  remove(id: string) {
+  remove(id: string): Promise<null> {
     return this.repository.findByIdAndDelete(id);
   }
 }
